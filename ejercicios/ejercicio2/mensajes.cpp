d3c6bc069185fda5a323eeeabd4b5eef9c348f8b
Alison Pineda <nicollephdz@gmail.com>
2022-02-02 21:10:30 -0600

    agregando ejercicio 2

---
+++ ejercicios/ejercicio2/mensajes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  string nombre; 
+  cout << "ingrese su nombre";
+
+   cin >> nombre;
+   cout << endl;
+   cout << "holi, mi nombre es: " << nombre;
+   cout << endl;
+    
+    return 0;
+}
