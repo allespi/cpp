f805a5f9f0833584128655db848a9e8fd9c736da
Alison Pineda <nicollephdz@gmail.com>
2022-02-07 20:27:26 -0600

    main

---
+++ ejercicios/ejercicio3/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int NumeroEntero;
+    NumeroEntero = 15;
+    cout << "Entero" << NumeroEntero<<endl;
+
+    NumeroEntero = 18;
+    cout << "Entero modificado" << NumeroEntero<<endl;
+
+
+
+    return 0;
+}
