c8ac4a908cb10c4b9cc3df97f34b34aa47a48a7c
allespi <nicollephdz@gmail.com>
2022-02-08 20:15:15 -0600

    ejercicio4

---
+++ ejercicios/ejercicio4/variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorboolean;
+
+    valorboolean = true;
+
+
+    int valorEntero = 15;
+
+    double valordouble = 20.99;
+
+    string valorstring = "hola, como estan";
+
+    char valorchar = 'b';  
+
+    cout << "valor boolean"<< valorboolean <<endl;
+    cout << "valor entero"<< valorEntero <<endl;
+    cout << "valor double"<< valorstring <<endl;
+    cout << "valor char"<< valorchar <<endl;
+
+    return 0;
+
+}
+
