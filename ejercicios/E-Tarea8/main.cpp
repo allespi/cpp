238b958ff1fd2d40a5324828ae8933c7cabecbd5
allespi <nicollephdz@gmail.com>
2022-03-23 20:11:34 -0600

    raiz cuadrada de un numero

---
+++ ejercicios/E-Tarea8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double n1 = 0;
+    double n2 = 0;
+    
+    //La función sqrt calcula la raíz cuadrada de un numero dado 
+
+    n2 = sqrt (n1);
+
+    cout << "El numero  " << n1 << "  tiene como raiz cuadrada: " << n2 << endl;
+    
+    return 0;
+}
